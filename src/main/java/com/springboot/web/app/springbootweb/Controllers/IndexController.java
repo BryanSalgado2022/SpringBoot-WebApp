1870439f702846899272e2abd20620834db7ef3b
BryanSalgadoCode <bsalgado@itstrategys.com>
2023-10-24 09:51:43 -0500

    Model Objects

--- src/main/java/com/springboot/web/app/springbootweb/Controllers/IndexController.java
+++ src/main/java/com/springboot/web/app/springbootweb/Controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.springboot.web.app.springbootweb.Controllers;
 
+import com.springboot.web.app.springbootweb.Models.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,5 +14,15 @@ public class IndexController {
     public String index(Model model){
         model.addAttribute("titulo", "Hola Spring Framework!");
         return "index";
+    }
+
+    @RequestMapping("/perfil")
+    public String Perfil(Model model){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Bryan");
+        usuario.setApellido("Guzman");
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+        return "perfil";
     }
 }
